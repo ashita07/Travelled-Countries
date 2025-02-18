5ee06007898eab1bf2ca24fbef00bc35d580d268
Ashita <ashitam2407@gmail.com>
2025-02-18 14:15:52 +0530

    added lazyLoading

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
+import { CityContextProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProctectedRoute from "./pages/ProctectedRoute";
+
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-// import { useEffect, useState } from "react";/
-import Product from "./pages/Product copy";
-import Pricing from "./pages/Pricing copy";
-import HomePage from "./pages/Homepage copy";
-import PageNotFound from "./pages/PageNotFound copy";
-import AppLayout from "./pages/AppLayout";
-import Login from "./pages/Login";
+// import Product from "./pages/Product copy";
+// import Pricing from "./pages/Pricing copy";
+// import HomePage from "./pages/Homepage copy";
+// import PageNotFound from "./pages/PageNotFound copy";
+// import AppLayout from "./pages/AppLayout";
+// import Login from "./pages/Login";
+// import Login from "./pages/Login";
 import CountryList from "./components/CountryList";
 import CityList from "./components/CityList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { CityContextProvider } from "./contexts/CitiesContext";
-import { AuthProvider } from "./contexts/FakeAuthContext";
-import ProctectedRoute from "./pages/ProctectedRoute";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+import { lazy, Suspense } from "react";
+
+const HomePage = lazy(() => import("./pages/Homepage copy"));
+const Pricing = lazy(() => import("./pages/Pricing copy"));
+const Login = lazy(() => import("./pages/Login"));
+const Product = lazy(() => import("./pages/Product copy"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound copy"));
 
 // import PageNav from "./components/PageNav";
 
@@ -22,34 +32,36 @@ export default function App() {
       <CityContextProvider>
         <AuthProvider>
           <BrowserRouter>
-            <Routes>
-              <Route index element={<HomePage />} />
-              <Route path="*" element={<PageNotFound />} />
-              <Route path="product" element={<Product />} />
-              <Route path="pricing" element={<Pricing />} />
-              <Route path="/login" element={<Login />} />
-              <Route
-                path="applayout"
-                element={
-                  <ProctectedRoute>
-                    <AppLayout />
-                  </ProctectedRoute>
-                }
-              >
-                <Route index element={<Navigate replace to="cities" />} />
+            <Suspense fallback={<SpinnerFullPage></SpinnerFullPage>}>
+              <Routes>
+                <Route index element={<HomePage />} />
+                <Route path="*" element={<PageNotFound />} />
+                <Route path="product" element={<Product />} />
+                <Route path="pricing" element={<Pricing />} />
+                <Route path="/login" element={<Login />} />
                 <Route
-                  path="cities"
+                  path="applayout"
                   element={
-                    <p>
-                      <CityList />
-                    </p>
+                    <ProctectedRoute>
+                      <AppLayout />
+                    </ProctectedRoute>
                   }
-                />
-                <Route path="cities/:id" element={<City />} />
-                <Route path="countries" element={<CountryList />} />
-                <Route path="form" element={<Form />} />
-              </Route>
-            </Routes>
+                >
+                  <Route index element={<Navigate replace to="cities" />} />
+                  <Route
+                    path="cities"
+                    element={
+                      <p>
+                        <CityList />
+                      </p>
+                    }
+                  />
+                  <Route path="cities/:id" element={<City />} />
+                  <Route path="countries" element={<CountryList />} />
+                  <Route path="form" element={<Form />} />
+                </Route>
+              </Routes>
+            </Suspense>
           </BrowserRouter>
         </AuthProvider>
       </CityContextProvider>
